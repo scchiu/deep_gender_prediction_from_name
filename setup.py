60f8d502eab834ac50a6a518fea8b16cfe96c45e
Steve Chiu <steve.scchiu@gmail.com>
2018-11-27 14:11:38 +0800

    - add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md','r') as f:
+    long_description=f.read()
+
+setup(
+        name='deep_gender_prediction_from_name',
+        version='1.0',
+        license='MIT',
+        long_description=long_description,
+        author='Steve Chiu',
+        author_email='steve.scchiu@gmail.com',
+        packages=['dgpn'],
+        install_requires=[
+            'keras',
+            'tensorflow',
+            'pandas',
+            'sklearn'
+            ],
+        scripts=[]
+)
